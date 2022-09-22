a52c3cf4026c7ac44e6a652c6703954c822484d2
sharglut <p.filimon75@gmail.com>
2022-09-22 21:05:03 +0200

    logic ajouter operation terminee

--- src/assets/js/dataGraph.js
+++ src/assets/js/dataGraph.js
@@ -1,6 +1,7 @@
 // <block:setup:1>
-const datapoints = [1200, 750, 775, 760, 2560];
-const DATA_COUNT = datapoints.length + 2;
+const balanceArray = [1200, 750, 775, 760, 2560]; // old const datapoints
+const DATA_COUNT = balanceArray.length + 2; // old const datapoints
+// TODO dynamic length calcul
 const labels = [];
 for (let i = 0; i < DATA_COUNT; ++i) {
   labels.push(i.toString());
@@ -10,7 +11,7 @@ const data = {
   datasets: [
     {
       label: "Compte",
-      data: datapoints,
+      data: balanceArray, // old : datapoints
       borderColor: "purple",
       // fill: true,
       cubicInterpolationMode: "monotone",
@@ -78,31 +79,84 @@ function addTemperature(time, temperature) {
 const selectElement = (element) => {
   return document.querySelector(`${element}`);
 };
+const balance = selectElement("#solde");
 const operationForm = selectElement("#operationForm");
 const operationTypeSelector = selectElement("#operator");
 const operationTitle = selectElement("#titre");
 const operationDescription = selectElement("#desc");
 const operationAmount = selectElement("#montant");
-const formSubmit = selectElement(".btSubmit");
+// const formSubmit = selectElement(".btSubmit");
 
-const gridOperationContainer = selectElement(".grid-container");
-const operationBlock = selectElement(".operation");
+const gridOperationContainer = document.querySelectorAll(".grid-container")[1];
+// const operationBlock = selectElement(".operation");
+
+// format number to 1 000 instead 1000
+const addSpaceInNumber = (num) => {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+};
 
 operationForm.addEventListener("submit", (e) => {
-  // const mainDiv = document.createElement('div')
-  // mainDiv.classList.add("operation", "credit")
-  // const gridDiv = document.createElement("div").appendChild(mainDiv)
-  // gridDiv.classList.add("grid-x", "grid-padding-x", "align-middle");
-  // const cellDiv = document.createElement("div").appendChild(gridDiv)
-  // cellDiv.classList.add("cell", "shrink");
-  // const pictoDiv = document.createElement("div")
   e.preventDefault();
-  const newOperationBlock = gridOperationContainer.appendChild(
-    operationBlock.cloneNode()
+
+  let updatedBalance = balanceArray[balanceArray.length - 1].toFixed(2);
+  let formatedBalance = addSpaceInNumber(updatedBalance);
+  balance.innerHTML = `${formatedBalance}€`;
+
+  const mainDiv = document.createElement("div");
+  gridOperationContainer.appendChild(mainDiv);
+  mainDiv.classList.add(
+    "operation",
+    `${operationTypeSelector.value === "credit" ? "credit" : "debit"}`
   );
-  console.log(newOperationBlock);
+  const gridDiv = document.createElement("div");
+  mainDiv.appendChild(gridDiv);
+  gridDiv.classList.add("grid-x", "grid-padding-x", "align-middle");
+  const pictoCell = document.createElement("div");
+  gridDiv.appendChild(pictoCell);
+  pictoCell.classList.add("cell", "shrink");
+  const pictoDiv = document.createElement("div");
+  pictoCell.appendChild(pictoDiv);
+  pictoDiv.classList.add("picto");
+  const pictoImg = document.createElement("img");
+  pictoDiv.appendChild(pictoImg);
+  pictoImg.src =
+    operationTypeSelector.value === "credit"
+      ? "./assets/images/sac-dargent.png"
+      : "./assets/images/depenses.png";
+  pictoImg.setAttribute(
+    "alt",
+    `${operationTypeSelector.value === "credit" ? "credit" : "debit"}`
+  );
+  const infoCell = document.createElement("div");
+  gridDiv.appendChild(infoCell);
+  infoCell.classList.add("cell", "auto");
+  const infoTitle = document.createElement("h2");
+  infoCell.appendChild(infoTitle);
+  infoTitle.innerHTML = `${operationTitle.value}`;
+  const infoSpan = document.createElement("small");
+  infoCell.appendChild(infoSpan);
+  infoSpan.innerHTML = `${operationDescription.value}`;
+  const amountCell = document.createElement("div");
+  gridDiv.appendChild(amountCell);
+  amountCell.classList.add("cell", "small-3", "text-right");
+  const amountValue = document.createElement("p");
+  amountCell.appendChild(amountValue);
+  amountValue.classList.add("count");
+  amountValue.innerHTML = `${addSpaceInNumber(operationAmount.value)}€`;
+  let amountPercent = document.createElement("small");
+  amountCell.appendChild(amountPercent);
+  console.log(operationAmount.value);
+  console.log(balanceArray[balanceArray.length - 1]);
+  amountPercent.innerHTML = `${(
+    (parseInt(operationAmount.value) / balanceArray[balanceArray.length - 1]) *
+    100
+  ).toFixed(2)}%`;
+
+  operationTypeSelector.value === "credit"
+    ? balanceArray.push(
+        balanceArray[balanceArray.length - 1] + parseInt(operationAmount.value)
+      )
+    : balanceArray.push(
+        balanceArray[balanceArray.length - 1] - parseInt(operationAmount.value)
+      );
 });
-const newOperationBlock = gridOperationContainer.appendChild(
-  operationBlock.cloneNode()
-);
-console.log(gridOperationContainer);
